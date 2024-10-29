ea10e57f4c0b2e22cab3f943e79a597e33d7bd29
Alaa Samir <alaasamir977@gmail.com>
2024-10-29 23:39:42 +0300

    add InfraStructre layer

--- DomainLayer/Category.cs
+++ DomainLayer/Category.cs
@@ -10,5 +10,6 @@ namespace DomainLayer
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public ICollection<Product> Products { get; set; }
     }
 }
---
+++ InfraStructureLayer/DBContext/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DomainLayer;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace InfraStructureLayer.DBContext
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext()
+        {
+           
+        }
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        public virtual DbSet<Product> Product { get; set; }
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<CartItem> CartItem { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasOne<Category>(s => s.Category)
+                .WithMany(g => g.Products)
+                .HasForeignKey(s => s.CategoryId);
+        }
+    }
+}
